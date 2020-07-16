758ebcc1f723d421b51ade88b954771485419bb4
KotKat <kotkat.lv@gmail.com>
2020-07-16 16:04:00 +0300

    Added comments for Vectos.cs

--- ThreeDimensionalVector/Vector.cs
+++ ThreeDimensionalVector/Vector.cs
@@ -5,14 +5,32 @@ namespace ThreeDimensionalArray
 {
     public class Vector
     {
+        /// <summary>
+        /// X coordinate
+        /// </summary>
         public double X { get; private set; } = 0;
 
+        /// <summary>
+        /// Y coordinate
+        /// </summary>
         public double Y { get; private set; } = 0;
 
+        /// <summary>
+        /// Z coordinate
+        /// </summary>
         public double Z { get; private set; } = 0;
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public Vector() { }
 
+        /// <summary>
+        /// A parameterized constructor that accepts the coordinates of the vertices
+        /// </summary>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
+        /// <param name="z">Z coordinate</param>
         public Vector(double x, double y, double z)
         {
             X = x;
@@ -20,22 +38,69 @@ namespace ThreeDimensionalArray
             Z = z;
         }
 
+        /// <summary>
+        /// Vector length
+        /// </summary>
         public double Length => Math.Round(Math.Sqrt(X * X) + (Y * Y) + (Z * Z), 2);
 
+        /// <summary>
+        /// Overloaded unary operator "+" for three-dimensional vector
+        /// </summary>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator +(Vector vectorA) => new Vector(vectorA.X, vectorA.Y, vectorA.Z);
 
+        /// <summary>
+        /// Overloaded unary operator "-" for three-dimensional vector
+        /// </summary>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator -(Vector vectorA) => new Vector(-vectorA.X, -vectorA.Y, -vectorA.Z);
 
+        /// <summary>
+        /// Overloaded unary operator "++" for three-dimensional vector
+        /// </summary>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator ++(Vector vectorA) => new Vector(++vectorA.X, ++vectorA.Y, ++vectorA.Z);
 
+        /// <summary>
+        /// Overloaded unary operator "--" for three-dimensional vector
+        /// </summary>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator --(Vector vectorA) => new Vector(--vectorA.X, --vectorA.Y, --vectorA.Z);
 
+        /// <summary>
+        /// Overloaded binary operator "+" for three-dimensional vectors
+        /// </summary>
+        /// <param name="vectorA">First three-dimensional vector object</param>
+        /// <param name="vectorB">Second three-dimensional vector object</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator +(Vector vectorA, Vector vectorB) => new Vector(vectorA.X + vectorB.X, vectorA.Y + vectorB.Y, vectorA.Z + vectorB.Z);
 
+        /// <summary>
+        /// Overloaded binary operator "-" for three-dimensional vectors
+        /// </summary>
+        /// <param name="vectorA">First three-dimensional vector object</param>
+        /// <param name="vectorB">Second three-dimensional vector object</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator -(Vector vectorA, Vector vectorB) => new Vector(vectorA.X - vectorB.X, vectorA.Y - vectorB.Y, vectorA.Z - vectorB.Z);
 
+        /// <summary>
+        /// Overloaded binary operator "*" for three-dimensional vectors
+        /// </summary>
+        /// <param name="vectorA">First three-dimensional vector object</param>
+        /// <param name="vectorB">Second three-dimensional vector object</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator *(Vector vectorA, Vector vectorB) => new Vector((vectorA.Y * vectorB.Z) - (vectorA.Z * vectorB.Y), (vectorA.Z * vectorB.X) - (vectorA.X * vectorB.Z), (vectorA.X * vectorB.Y) - (vectorA.Y * vectorB.X));
 
+        /// <summary>
+        /// Overloaded binary operator "/" for three-dimensional vectors
+        /// </summary>
+        /// <param name="vectorA">First three-dimensional vector object</param>
+        /// <param name="vectorB">Second three-dimensional vector object</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator /(Vector vectorA, Vector vectorB)
         {
             if (IsZero(vectorB.X, vectorB.Y, vectorB.Z))
@@ -46,12 +111,36 @@ namespace ThreeDimensionalArray
             return new Vector(Math.Round(vectorA.X / vectorB.X, 2), Math.Round(vectorA.Y / vectorB.Y, 2), Math.Round(vectorA.Z / vectorB.Z, 2));
         }
 
+        /// <summary>
+        /// Overloaded binary operator "+" for three-dimensional vector and number
+        /// </summary>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <param name="value">Number</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator +(Vector vectorA, double value) => new Vector(vectorA.X + value, vectorA.Y + value, vectorA.Z + value);
 
+        /// <summary>
+        /// Overloaded binary operator "-" for three-dimensional vector and number
+        /// </summary>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <param name="value">Number</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator -(Vector vectorA, double value) => new Vector(vectorA.X - value, vectorA.Y - value, vectorA.Z - value);
 
+        /// <summary>
+        /// Overloaded binary operator "*" for three-dimensional vector and number
+        /// </summary>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <param name="value">Number</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator *(Vector vectorA, double value) => new Vector(vectorA.X * value, vectorA.Y * value, vectorA.Z * value);
 
+        /// <summary>
+        /// Overloaded binary operator "/" for three-dimensional vector and number
+        /// </summary>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <param name="value">Number</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator /(Vector vectorA, double value)
         {
             if (IsZero(value))
@@ -62,23 +151,71 @@ namespace ThreeDimensionalArray
             return new Vector(Math.Round(vectorA.X / value, 2), Math.Round(vectorA.Y / value, 2), Math.Round(vectorA.Z / value, 2));
         }
 
+        /// <summary>
+        /// Overloaded binary operator "%" for three-dimensional vector and number
+        /// </summary>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <param name="value">Number</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator %(Vector vectorA, double value) => new Vector(vectorA.X % value, vectorA.Y % value, vectorA.Z % value);
 
+        /// <summary>
+        /// Overloaded binary operator "+" for number and three-dimensional vector
+        /// </summary>
+        /// <param name="value">Number</param>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator +(double value, Vector vectorA) => vectorA + value;
 
+        /// <summary>
+        /// Overloaded binary operator "-" for number and three-dimensional vector
+        /// </summary>
+        /// <param name="value">Number</param>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator -(double value, Vector vectorA) => vectorA - value;
 
+        /// <summary>
+        /// Overloaded binary operator "*" for number and three-dimensional vector
+        /// </summary>
+        /// <param name="value">Number</param>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator *(double value, Vector vectorA) => vectorA * value;
 
+        /// <summary>
+        /// Overloaded binary operator "/" for number and three-dimensional vector
+        /// </summary>
+        /// <param name="value">Number</param>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator /(double value, Vector vectorA) => vectorA / value;
 
+        /// <summary>
+        /// Overloaded binary operator "%" for number and three-dimensional vector
+        /// </summary>
+        /// <param name="value">Number</param>
+        /// <param name="vectorA">Three-dimensional vector object</param>
+        /// <returns>Three-dimensional vector object</returns>
         public static Vector operator %(double value, Vector vectorA) => vectorA % value;
 
+        /// <summary>
+        /// Overloaded binary operator "==" for three-dimensional vectors
+        /// </summary>
+        /// <param name="vectorA">First three-dimensional vector</param>
+        /// <param name="vectorB">Second three-dimensional vector</param>
+        /// <returns>Boolean</returns>
         public static bool operator ==(Vector vectorA, Vector vectorB)
         {
             return !(vectorA is null) && !(vectorB is null) ? ReferenceEquals(vectorA, vectorB) || vectorA.Length == vectorB.Length : vectorA is null && vectorB is null;
         }
 
+        /// <summary>
+        /// Overloaded binary operator "!=" for three-dimensional vectors
+        /// </summary>
+        /// <param name="vectorA">First three-dimensional vector</param>
+        /// <param name="vectorB">Second three-dimensional vector</param>
+        /// <returns>Boolean</returns>
         public static bool operator !=(Vector vectorA, Vector vectorB) => !(vectorA == vectorB);
 
         public override bool Equals(object obj)
@@ -96,6 +233,11 @@ namespace ThreeDimensionalArray
 
         public override string ToString() => $"Vertex coordinates ({X}; {Y}; {Z})";
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
         private static bool IsZero(params double[] numbers) => numbers.Any(n => n == 0d);
     }
 }
